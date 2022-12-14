fece9977ee52d44c6293526304b381009578de43
Vasilii Zalizniak <vzalizniak31@gmail.com>
2022-12-14 14:00:34 +0200

    Removed unnecessary files and added logic to another files

--- js/script.js
+++ js/script.js
@@ -1,24 +1,35 @@
 const checkParams = () => {
-    const form = document.getElementById('form');
-    const phone = document.getElementById('pin');
-    const btn = document.getElementById('submit-btn');
-    const warningMessage = document.querySelector('.error-description');
+  const form = document.getElementById("form");
+  const phone = document.getElementById("pin");
+  const btn = document.getElementById("submit-btn");
+  const warningMessage = document.querySelector(".error-description");
 
-    phone.addEventListener('input', ({target}) => {
-        if (target.value === '+(381) 000-000-00') {
-            btn.removeAttribute('disabled');
-            phone.classList.replace('pin-code', 'error');
-            warningMessage.classList.add('visible');
-        } else if (
-            target.value === '+(381) 111-111-11') {
-            btn.removeAttribute('disabled');
-            form.addEventListener('submit', e => {
-                e.preventDefault();
-            });
-        } else {
-            btn.setAttribute('disabled', 'disabled');
-            warningMessage.classList.remove('visible');
-            phone.classList.replace('error', 'pin-code');
-        }
-    });
-}
+  phone.addEventListener("input", ({ target }) => {
+    if (target.value === "+(381) 000-000-00") {
+      btn.removeAttribute("disabled");
+      phone.classList.replace("pin-code", "error");
+      warningMessage.classList.add("visible");
+    } else if (target.value === "+(381) 111-111-11") {
+      btn.removeAttribute("disabled");
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+      });
+    } else {
+      btn.setAttribute("disabled", "disabled");
+      warningMessage.classList.remove("visible");
+      phone.classList.replace("error", "pin-code");
+    }
+  });
+};
+
+const dynamicMask = () =>
+  IMask(document.getElementById("pin"), {
+    mask: [
+      {
+        mask: "+(381) 000-000-00",
+      },
+      {
+        mask: /^\S*@?\S*$/,
+      },
+    ],
+  });
